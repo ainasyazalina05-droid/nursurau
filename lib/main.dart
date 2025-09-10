9dea4ee0979fa431de48b38f2f6c74c10a57f4dd
Nur Aina Syazalina <ainasyazalina05@gmail.com>
2025-09-11 01:15:28 +0800

    your message

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nursurau/pages/admin_ajk/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:nursurau/pages/admin_paid/paid.dart';
 import 'package:nursurau/pages/users/home_page.dart';
 import 'firebase_options.dart';
 
