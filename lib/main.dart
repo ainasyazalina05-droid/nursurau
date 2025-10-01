3c3270e478c4813a3fea7a438b89a6601799f130
Nur Aina Syazalina <ainasyazalina05@gmail.com>
2025-10-01 13:33:34 +0800

    your message

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart' show Firebase;
 import 'package:nursurau/pages/admin_ajk/login_page.dart' show LoginPage;
-<<<<<<< HEAD
 import 'package:nursurau/pages/users/home_page.dart' show HomePage;
-=======
 import 'package:nursurau/pages/users/home_page.dart';
->>>>>>> 9e5e2c5c63933b958b71a620ea140dc7999fe964
 import 'firebase_options.dart';
 
 // Admin pejabat
@@ -35,13 +32,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
 
-<<<<<<< HEAD
       // 👉 You switch this manually depending on role/page you want to run:
       home: const LoginPage(),
-=======
       // 👉 Change this to test different UIs
       //home: const HomePage(),
-      home: const HomePage(),
+      // home: const HomePage(),
       //home: const LoginPage(),
       // home: const AdminPaidPage(),
       // home: const LoginPage(),
@@ -50,7 +45,6 @@ class MyApp extends StatelessWidget {
       // home: selectedHome,
 
       // Example of old commented options kept:
->>>>>>> 9e5e2c5c63933b958b71a620ea140dc7999fe964
       // home: const HomePage(),
       // home: const AdminPaidPage(),
       // home: const AdminAjkLoginPage(),
